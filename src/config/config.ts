5de565dfab9c934f2f039b94ad59d5a2284a6fa1
Edin Mujagic <edinmujagic2@gmail.com>
2022-04-20 17:02:13 +0200

    Fix config issues

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,16 +1,10 @@
 class Config {
-    environment = 'test';
-
     getMongoConnectUri() {
-        if (/test/.test(this.environment)) {
-            // normally these values would be injected through env vars
-            const username = "admin" || process.env.DB_USER;
-            const password = "admin1234" || process.env.DB_PASS;
-            const hostname = "cluster0.2cacp.mongodb.net" || process.env.DB_HOST;
-            const dbname = "tictactoe" || process.env.DB_NAME;
-            return `mongodb+srv://${username}:${password}@${hostname}/${dbname}?retryWrites=true&w=majority`;
-        }
-        return '';
+        const username = process.env.DB_USER || "admin";
+        const password = process.env.DB_PASS || "admin1234";
+        const hostname = process.env.DB_HOST || "cluster0.2cacp.mongodb.net";
+        const dbname = process.env.DB_NAME || "tictactoe";
+        return `mongodb+srv://${username}:${password}@${hostname}/${dbname}?retryWrites=true&w=majority`;
     }
 }
 
